2dc29047a278871cdea033081de5504793adf2d9
Steve Good <steve@stevegood.org>
2014-01-15 09:37:41 -0600

    Added URL where Items.xml can be obtained

--- ParseRiftItems.groovy
+++ ParseRiftItems.groovy
@@ -4,6 +4,9 @@ import org.stevegood.rift.RiftItemSlurper
  * User: stgood
  * Date: 1/9/14
  * Time: 11:35 AM
+ *
+ * Download and unzip the Items.xml file from Rift_Discoveries_{file_date} @ ftp://ftp.trionworlds.com/rift/data/
+ *
  */
 
 def start = new Date().time
